e5692e5ce93a8c85acce0fab0cd55e29ec9e8324
gadapuharish <harishgadapu192@gmail.com>
2025-12-29 12:27:52 +0530

    Add files via upload

---
+++ Retail_Shop2.java
@@ -0,0 +1,15 @@
+public class Retail_Shop2
+{
+public static void main(String args[])
+{
+int Invoice_ID = 767393;
+int Product_ID = 773498734;
+double Product_Cost = 200.34676734;
+int Quantity = 45;
+double Discount = 45.77777;
+double Total_Price = Product_Cost - Discount ;
+boolean  feedback;
+System.out.println(Total_Price);
+}
+}
+
