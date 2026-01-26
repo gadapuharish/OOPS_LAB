926bff45ddfae8a45bf928c53764296bf3806ba4
gadapuharish <harishgadapu192@gmail.com>
2026-01-26 21:40:29 +0530

    Add files via upload

---
+++ IntInput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class IntInput 
+{
+public static void main (String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter any number: ");
+int a = sc.nextInt();
+System.out.println("You entered: "+a);
+}
+}
